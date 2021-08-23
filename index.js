5885b00e5487ef12ddf59d4bcd354dde40469c41
ser veresta <subramaniang573@gmail.com>
2021-08-23 15:26:11 +0530

    -node description

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ const URL = process.env.MONGODB_URI || "mongodb+srv://gopal:1213@cluster0.azvtw.
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send(`
+  /mentors/ - to view all the mentors
+  /mentors/create - to create new mentor
+  /mentors/assign?students=s1,s2&mentor=m1 - to assign students s1 and s2 to mentor m1
+  /mentors/students/m1 - to view the students of mentor m1
+
+  /students/ - to view all the students
+  /studetns/create - to create new student 
+  /students/assign?student=s1&mentor=m2 - to change the mentor of student s1 to mentor m2
+  `);
+});
+
 app.use("/mentors", mentorRouter);
 app.use("/students", studentRouter);
 
